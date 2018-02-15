a76ef5ac24f14a0486fa833569976c9a0fd847f6
Ousalah <mohamed22oussalah@gmail.com>
2018-02-15 16:23:34 +0000

    add no completed lectures

--- youtube-playlists-progression.js
+++ youtube-playlists-progression.js
@@ -8,19 +8,19 @@ for (var i = 0, len = lectures.length; i < len; i++) {
 var lectures_hours = Math.floor( lectures_total_time / 60);
 var lectures_minutes = lectures_total_time % 60;
 lectures_hours = lectures_hours < 10 ? "0" + lectures_hours : lectures_hours;
-lectures_minutes = lectures_minutes < 10 ? lectures_minutes + "0" : lectures_minutes;
+lectures_minutes = lectures_minutes < 10 ? "0" + lectures_minutes : lectures_minutes;
 
-completed = document.querySelectorAll("#overlays #progress");
+watched = document.querySelectorAll("#overlays #progress");
 completed_total_time = 0;
 completed_count = 0;
-for (var i = 0, len = completed.length; i < len; i++) {
-	if(completed[i].style.width == '100%'){
-		completed_total_time += parseFloat(completed[i].parentNode.parentNode.childNodes[1].childNodes[0].innerHTML);
+for (var i = 0, len = watched.length; i < len; i++) {
+	if(watched[i].style.width == '100%'){
+		completed_total_time += parseFloat(watched[i].parentNode.parentNode.childNodes[1].childNodes[0].innerHTML);
 		completed_count++;
 	}
 }
 var completed_hours = Math.floor( completed_total_time / 60);
 var completed_minutes = completed_total_time % 60;
 completed_hours = completed_hours < 10 ? "0" + completed_hours : completed_hours;
-completed_minutes = completed_minutes < 10 ? completed_minutes + "0" : completed_minutes;
-alert(completed_count + "/" + lectures_count + " lectures   |   " + completed_hours + ":" + completed_minutes + "/" + lectures_hours + ":" + lectures_minutes + " hours");
+completed_minutes = completed_minutes < 10 ? "0" + completed_minutes : completed_minutes;
+console.log(completed_count + "/" + lectures_count + " completed lectures \n\n" + completed_hours + ":" + completed_minutes + "/" + lectures_hours + ":" + lectures_minutes + " completed hours \n\n" + (watched.length - completed_count) + " no completed lectures");
